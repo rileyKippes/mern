02d92382cc6d28abb1db4322b8de1febf9a57c7b
rileyKippes <rileykippes@gmail.com>
2020-09-17 19:16:55 -0500

    Added tabs to direct messages

--- src/portfolio/messages/messages.js
+++ src/portfolio/messages/messages.js
@@ -1,101 +1,144 @@
 class Messages extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {	ready: false,
-					data: {},
-					user: {}};
-	this.setUser();
-	this.getAPI();
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			ready: false,
+			data: {},
+			user: {}
+		};
+		this.setUser();
+		this.getAPI(); //doesn't need to refresh like chat.js does
+	}
 
-	setUser(){
+	setUser() {
 		fetch('/u/api')
-		  .then((response) => {
-			return response.json();
-		  })
-		  .then((myJson) => {
-			//part of initialization
-			//so we can't use setState
-			this.state = { ready: true, data:this.state.data, user: myJson};
-		  });
+			.then((response) => {
+				return response.json();
+			})
+			.then((myJson) => {
+				//part of initialization
+				//so we can't use setState
+				this.state = { ready: true, data: this.state.data, user: myJson };
+			});
 	}
 
-	getAPI(){
+	getAPI() {
 		fetch('/p/messages/api')
-		  .then((response) => {
-			return response.json();
-		  })
-		  .then((myJson) => {
-			this.setState({ ready: true, data:myJson, user: this.state.user});
-		  });
-		
+			.then((response) => {
+				return response.json();
+			})
+			.then((myJson) => {
+				this.setState({ ready: true, data: myJson, user: this.state.user });
+			});
+
 	}
 
-	
 
-	postMessage(){
+
+	postMessage() {
 		const form = new FormData(document.getElementById("comment_box"));
-		fetch('/p/messages/api',  {
-			  method: 'POST', 
-			  body: form }
-			).then((response) => {
+		fetch('/p/messages/api', {
+			method: 'POST',
+			body: form
+		}
+		).then((response) => {
 			return response.json();
-		  })
-		  .then((myJson) => {
-			this.setState({ ready: true, data:myJson});
-		  });
+		})
+			.then((myJson) => {
+				this.setState({ ready: true, data: myJson });
+			});
 	}
 
 	render() {
 
-	setTimeout(this.getAPI.bind(this),2000);
-	if (this.state.ready) {
-		var messages = [];
-		for (var data of this.state.data) {
-			console.log(data);
-			var currStyle = {
-				borderColor:data.color
-			};
-			messages.push( (
-				<div 	className="message"
-						key={data._id} 
-						style={currStyle}> 
-						{data.sender} :					
-						{data.newMessage} 
-				</div> 
-			));
+		//needs css
+
+		//first build senders list and messages
+		//then build sender tabs with messages inserted
+		//it's more modular that way
+		if (this.state.ready) {
+			var messages = [];
+			var sendersList = [];
+			var senderTabs = [];
+			for (var data of this.state.data) {
+				var currStyle = {
+					borderColor: data.color
+				};
+
+				function checkSender(sender) {
+					console.log(""+(sender == data.sender).toString() + " = "+sender+" == "+data.sender );
+					return sender === data.sender;
+				}
+				console.log(data);
+				console.log("Index:"+sendersList.indexOf(checkSender));
+				console.log("DS IO:"+sendersList.indexOf(data.sender));
+				if (!sendersList.includes(data.sender)) {
+					sendersList.push(data.sender);
+					var index = sendersList.indexOf(data.sender);
+					console.log("Sender " + data.sender + " not found, new index is "+index);
+					messages[index] = [];
+					messages[index].push((
+						<div className="message"
+							key={data._id}
+							style={currStyle}>
+							{data.newMessage}
+						</div>
+					));
+				}
+				else {
+					messages[sendersList.indexOf(data.sender)].push((
+						<div className="message"
+							key={data._id}
+							style={currStyle}>
+							{data.newMessage}
+						</div>
+					));
+				}
+			}
+			
+			for(var i = 0; i < sendersList.length; i++){
+				senderTabs.push(
+					<div key={sendersList[i]}
+						className="senderTab">
+						{sendersList[i]}
+						{messages[i]}
+					</div>
+				);
+			}
+
+
+			return (
+				<div id="messages_div">
+					<form id="comment_box" onSubmit={this.postMessage.bind(this)}>
+						<input id="comment_input"
+							type="text"
+							placeholder="Message Goes Here"
+							name="newMessage">
+						</input>
+						<input id="reciever_input"
+							type="text"
+							placeholder="Send to"
+							name="reciever">
+						</input>
+						<input id="post_message_btn"
+							type="button"
+							value=" > "
+							onClick={this.postMessage.bind(this)}>
+						</input>
+					</form>
+					<div id="message_box">
+						{senderTabs}
+					</div>
+				</div>
+			);
 		}
-	return (
-		<div id="messages_div">
-			<form id="comment_box" onSubmit={this.postMessage.bind(this)}>
-				<input 	id="comment_input"
-						type="text"
-						placeholder="Message Goes Here"
-						name="newMessage">
-			  	</input>
-				<input 	id="reciever_input"
-						type="text"
-						placeholder="Send to"
-						name="reciever">
-			  	</input>
-				<input id="post_message_btn"	
-						type="button"
-						value=" > "
-						onClick={this.postMessage.bind(this)}>
-				</input>
-			</form>
-			<div id="message_box">
-				{messages}
+		//does the following if ready is false. 
+		return (
+			<div id="messages_div">
+				Loading. . .
 			</div>
-		</div>
 		);
-    }
-    return (
-		<div id="messages_div">
-		Loading. . .
-		</div>
-    );
-  }
+	}
 }
 
 let MessageDomContainer = document.querySelector('#messages_container');
