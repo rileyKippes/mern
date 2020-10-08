9b71126cb95e3f3d185c769c9904fa65be58cd98
rileyKippes <rileykippes@gmail.com>
2020-10-08 12:49:49 -0500

    Register User Upgraded  Messages

--- routes/user/register_api.js
+++ routes/user/register_api.js
@@ -11,52 +11,56 @@ var collName = 'users';
 
 var saltRounds = 10;
 
-function generateColor(){
-	var hex = ['0','a','f'];
+function generateColor() {
+	var hex = ['0', 'a', 'f'];
 	var ret = '#';
 	var rand;
-	for(var i = 0; i < 6; i++){
+	for (var i = 0; i < 6; i++) {
 		rand = Math.floor(Math.random() * hex.length);
 		ret += hex[rand];
 	}
 	return ret;
 }
 
-router.post('/',function (req,res) {
+router.post('/', function (req, res) {
 	console.log('Register api');
 	console.log(JSON.stringify(req.body));
-	if(req.body.password !== req.body.confirm_password){
-		console.log("Passwords do not match");
-		res.sendStatus(422);
+	if (req.body.password !== req.body.confirm_password) {
+		console.log("Passwords do not match"); //accurately gets here
+		res.redirect('/u/register?err=password_equality'); 
 	}
 	else {
-		var client = new mongo(url,{ useUnifiedTopology: true });
+		var client = new mongo(url, { useUnifiedTopology: true });
 		client.connect().then(() => {
 			return client.db(dbName);
 		}).then((db) => {
-		//first check if there's already a user with that username
-		const collection = db.collection(collName);
-		if(collection.find({username:req.body.username}).count() > 0)
-		{  
-			console.log("User already exists");
-			var user = collection.findOne({username:req.body.username});
-			console.log(JSON.stringify(user));
-			res.sendStatus(422);
-		}
-		else {
-			bcrypt.hash(req.body.password, saltRounds).then((hash) => {
-			var color = generateColor();
-			collection.insertOne({
-							username:req.body.username,
-							password:hash,
-							color:color
-							}).then(() => {
-				res.sendStatus(201);
-			});
-			});
+			//first check if there's already a user with that username
+			const collection = db.collection(collName);
+			if (collection.find({ username: req.body.username }).count() > 0) {
+				console.log("User already exists"); //doesn't get here
+				var user = collection.findOne({ username: req.body.username });
+				console.log(JSON.stringify(user));
+				res.redirect( '/u/register?err=user_already_exists');
+			}
+			else {
+				bcrypt.hash(req.body.password, saltRounds).then((hash) => {
+					var color = generateColor();
+					//for testing only
+					console.log("Would have created a new user");
+					res.sendStatus(201);
+
+					/*collection.insertOne({
+									username:req.body.username,
+									password:hash,
+									color:color
+									}).then(() => {
+						return res.redirect(308,'/u/login'); //this end works perfectly
+					});*/
+				});
 			}
 		}).catch((err) => {
 			console.log(err);
+			return res.redirect(308, '/u/register?err=unknown_error');
 		});
 	}
 });
--- src/user/register.jsx
+++ src/user/register.jsx
@@ -7,32 +7,85 @@ class RegisterUserClass extends React.Component {
 	}
 
 	render() {
-		return (
-			<form action="/u/register/api" method="post">
-				<p> Please use a unique login </p>
-				<p> While I do salt and hash passwords, I am NOT touting myself a security expert. </p>
+		var form = (
+			<form action="/u/register/api" method="post" className="mx-3 px-3">
+				<p className="text-center"> Please use a unique login </p>
+				<p className="text-center"> While I do salt and hash passwords, I am NOT touting myself a security expert. </p>
 				<div className="form-group row" >
-					<label className="col-sm-2 col-form-label" for="username">Username</label>
-					<div className="col-sm-10">
-						<input className="form-control" id="username" type="text" name="username" placeholder="Username" required />
+					<label className="col-2 col-form-label text-right" htmlFor="username">Username</label>
+					<div className="col-10">
+						<input className="form-control"
+							id="username"
+							type="text"
+							name="username"
+							placeholder="Username"
+							required />
 					</div>
 				</div>
 				<div className="form-group row" >
-					<label className="col-sm-2 col-form-label" for="password">Password</label>
-					<div className="col-sm-10">
-						<input className="form-control" id="password" type="password" name="password" placeholder="Password" required />
+					<label className="col-2 col-form-label text-right" htmlFor="password">Password</label>
+					<div className="col-10">
+						<input className="form-control"
+							id="password"
+							type="password"
+							name="password"
+							placeholder="Password"
+							value={this.state.password}
+							required />
 					</div>
 				</div>
 				<div className="form-group row" >
-					<label className="col-sm-2 col-form-label" for="confirm_password">Confirm Password</label>
-					<div className="col-sm-10">
-						<input className="form-control" id="password" type="password" name="confirm_password" placeholder="Confirm Password" required />
+					<label className="col-2 col-form-label text-right" htmlFor="confirm_password">Confirm Password</label>
+					<div className="col-10">
+						<input className="form-control"
+							id="password"
+							type="password"
+							name="confirm_password"
+							placeholder="Confirm Password"
+							value={this.state.confirm_password}
+							required />
 					</div>
 				</div>
-				<input className="btn btn-primary"
-					type="submit"
-					value="Register" />
+				<div className="form-group row justify-content-center">
+					<input className="btn btn-primary col-6 "
+						type="submit"
+						value="Register" />
+				</div>
+				<p className="text-center"> If you already have an account, then you can login <a href="/u/login"> here </a></p>
 			</form>
+		);
+		const passwordsEqualityMessage = (
+			<div className="alert alert-danger alert-dismissible fade show">
+				Passwords must be the same
+				<button type="button" className="close" data-dismiss="alert">&times;</button>
+			</div>);
+		const userExistsMessage = (
+			<div className="alert alert-danger alert-dismissible fade show">
+				User already exists. Did you want to <a href="/u/login"> login? </a>
+				<button type="button" className="close" data-dismiss="alert">&times;</button>
+			</div>);
+
+		if (window.location.search.search("password_equality") != -1) {
+			return (
+				<div>
+					{passwordsEqualityMessage}
+					{form}
+				</div>
+			);
+		}
+		else if (window.location.search.search("user_already_exists") != -1) {
+			return (
+				<div>
+					{userExistsMessage}
+					{form}
+				</div>
+			);
+
+		}
+		return (
+			<div>
+				{form}
+			</div>
 		);
 	}
 }
