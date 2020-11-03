e63998e774133f800b16326fa31a5c485c05f800
rileyKippes <rileykippes@gmail.com>
2020-11-02 18:12:29 -0600

    Mongo Manager Update

--- routes/portfolio/chat/chat_api.js
+++ routes/portfolio/chat/chat_api.js
@@ -1,69 +1,34 @@
 'use strict'
 
+var db = require('../../mongo_manager');
 var express = require('express');
 var router = express.Router();
-var mongo = require('mongodb').MongoClient;
-var utils = require('../../utils');
 
-const config = utils.getConfig();
-var url = config.mongo.url;
-const dbname = 'chat';
 const collName = "comments";
 
-
 router.get('/', function (req, res) {
-	var client = new mongo(url, { useUnifiedTopology: true });
-	client.connect().then(() => {
-		return client.db(dbname);
-	}).then((db) => {
-		const collection = db.collection(collName);
-		collection.find().sort({ utc: -1 }).limit(15).toArray().then((docs) => {
-			res.status(200).json(docs);
-		}).catch((err) => {
-			console.error(err);
-			res.sendStatus(500);
-		});
-	}).catch((err) => {
-		console.error(err);
-		res.sendStatus(500);
-	});
-
+	var docs = {};
+	db.findSortandLimit(res, collName, {}, { utc: -1 }, 15);
 });
 
 //uses the logged in user's color now
 //it used to use a variable that was isolated to the chat script
 //but now it's integrated!
 router.post('/', function (req, res) {
-	var client = new mongo(url, { useUnifiedTopology: true });
-	client.connect().then(() => {
-		return client.db(dbname);
-	}).then((db) => {
-		var d = new Date();
-		var currUTC = Date.now();
-		const collection = db.collection(collName);
-		var newComment = "failed to post comment";
-		if (req.body != undefined) {
-			newComment = req.body.comment;
-		}
-		else {
-			utils.debug("Cookie:" + JSON.stringify(req.cookies));
-		}
-		// Insert a comment.
-		collection.insertOne({
-			comment: newComment,
-			utc: currUTC,
-			cookie: req.user.color
-		}).catch((err) => {
-			console.error(err);
-			res.sendStatus(500);
-		});
-	}).then(() => {
-		res.sendStatus(200);
-	}).catch((err) => {
-		console.error(err);
-		res.sendStatus(500);
+	var d = new Date();
+	var currUTC = Date.now();
+	var newComment = "failed to post comment";
+	if (req.body != undefined) {
+		newComment = req.body.comment;
+	}
+	else {
+		utils.debug("Cookie:" + JSON.stringify(req.cookies));
+	}
+	// Insert a comment.
+	db.insert(res, collName, {
+		comment: newComment,
+		utc: currUTC,
+		cookie: req.user.color
 	});
-
 });
-
 module.exports = router;
