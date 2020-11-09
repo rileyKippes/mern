7494759d5fd85591859490ab5376b95faa1a0b5d
rileyKippes <rileykippes@gmail.com>
2020-11-09 16:06:58 -0600

    api cleanup

--- routes/portfolio/chat/chat.js
+++ routes/portfolio/chat/chat.js
@@ -1,16 +1,33 @@
 'use strict'
-{
+
 var express = require('express');
 var router = express.Router();
 var utils = require('../../../ts_built/utils');
+var db = require('../../../ts_built/mongo_manager');
 
-var chat_api = require('./chat_api');
+const collName = "comments";
 
-router.get('/',function (req,res) {
-	res.send(utils.getBetterHTMLTemplate('portfolio/chat.html',{title:"Chat box"}));
+router.get('/', function (req, res) {
+	res.send(utils.getBetterHTMLTemplate('portfolio/chat.html', { title: "Chat box" }));
 });
 
-router.use('/api',chat_api);
-}
+router.get('/api', function (req, res) {
+	db.findSortandLimit(collName, {}, { utc: -1 }, 15).then((ret) => { res.json(ret); });
+});
+
+router.post('/api', function (req, res) {
+	var currUTC = Date.now();
+	var newComment = req.body.comment ?? "Failed to post comment";
+	var color = '#000';
+	if (req.user) {
+		color = req.user.color;
+	}
+	// Insert a comment.
+	db.insert(collName, {
+		comment: newComment,
+		utc: currUTC,
+		cookie: color
+	}).then((ret) => { res.status(200); });
+});
 
 module.exports = router;
--- routes/portfolio/messages/messages.js
+++ routes/portfolio/messages/messages.js
@@ -1,16 +1,28 @@
 'use strict'
-{
 var express = require('express');
 var router = express.Router();
 var utils = require('../../../ts_built/utils');
+var db = require('../../../ts_built/mongo_manager');
 
-var message_api = require('./messages_api');
+const collName = "messages";
 
-router.get('/',function (req,res) {
-	res.send(utils.getBetterHTMLTemplate('portfolio/messages.html',{title:"Direct Messages"}));
+router.get('/', function (req, res) {
+	res.send(utils.getBetterHTMLTemplate('portfolio/messages.html', { title: "Direct Messages" }));
 });
 
-router.use('/api',message_api);
-}
+//get messages
+router.get('/api', function (req, res) {
+	db.findSortandLimit(collName, { $or: [{ sender: req.user.username }, { reciever: req.user.username }] }, { utc: -1 }, 0).then((ret) => { res.json(ret); });
+});
 
+//post messages
+router.post('/api', function (req, res) {
+	db.insert(collName, {
+		newMessage: req.body.newMessage,
+		utc: Date.now(),
+		sender: req.user.username,
+		reciever: req.body.reciever,
+		color: req.user.color
+	}).then(() => { res.sendStatus(200); });
+});
 module.exports = router;
